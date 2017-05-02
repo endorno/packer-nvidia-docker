ea307b8f8433614ee1f85fa2d7b96a1b476b15cf
Teppei Fujisawa <teppei.fujisawa@gmail.com>
2017-05-02 18:27:03 +0900

    for creating on p2 instance

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 apt-get update
-apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade                                    
+#apt-get upgrade -y
 echo "UseDNS no" >> /etc/ssh/sshd_config
